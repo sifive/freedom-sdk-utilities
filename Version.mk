30fb6fde29cf15eb3232c04f37b3a96b6d8faed9
cgsfv <cgsfv@users.noreply.github.com>
2021-01-08 11:32:16 -0800

    Bumping riscv-isa-sim submodule to latest commit

--- Version.mk
+++ Version.mk
@@ -2,8 +2,8 @@
 RISCV_ISA_SIM_VERSION := 1.0.1
 
 # Customization ID, which should identify the customization added to the original by SiFive
-FREEDOM_SDK_UTILITIES_ID := 2020.12.0-preview1
+FREEDOM_SDK_UTILITIES_ID := 2020.12.0-preview2
 
 # Characteristic tags, which should be usable for matching up providers and consumers
-FREEDOM_SDK_UTILITIES_RISCV_TAGS = rv32i rv64i m a f d c v zfh
+FREEDOM_SDK_UTILITIES_RISCV_TAGS = rv32i rv64i m a f d c v b zfh
 FREEDOM_SDK_UTILITIES_TOOLS_TAGS = dtc libfdt zspike-dasm
